b9a0c460f8cd3a75ed4ff15b88b467c5ab7e205e
Valentino Grassi <valengrassi7@gmail.com>
2025-07-21 12:00:48 -0300

    21/7

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,12 @@
 import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
-
+import md from 'vite-plugin-md';
 import preact from "@astrojs/preact";
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind(), preact()],
+  vite: {
+    plugins: [md()],
+  },
 });
