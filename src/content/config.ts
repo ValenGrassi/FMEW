b7f1ea3779d81d666c3db653ecad939286be1bbf
Valentino Grassi <valengrassi7@gmail.com>
2025-07-27 17:21:17 -0300

    config 4

--- src/content/config.ts
+++ src/content/config.ts
@@ -3,14 +3,9 @@ import { defineCollection, z } from "astro:content";
 const noticias = defineCollection({
   schema: z.object({
     title: z.string(),
+    description: z.string(),
     date: z.string(), 
     image: z.string().optional(),
-    buttons: z.array(
-      z.object({
-        label: z.string(),
-        url: z.string(),
-      })
-    ).optional(),
     body: z.string().optional()
   }),
 });
